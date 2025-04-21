d48d304c4ca8dd9c201bf63209af96bc74a2c615
ethanabebrown <127771118+ethanabebrown@users.noreply.github.com>
2025-04-21 10:49:43 -0400

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -7,8 +7,9 @@ const port = process.env.PORT || 3001;
 const cors = require('cors');
 app.use(cors());
 
-const CLIENT_ID = 'c89c120f-791b-4826-856c-c09659225db7'; // ← replace this
+const CLIENT_ID = 'c89c120f-791b-4826-856c-c09659225db7'; 
 const REDIRECT_URI = 'https://test-cuvl.onrender.com/callback';
+let accessToken = null;
 
 app.get('/launch', (req, res) => {
   const state = Math.random().toString(36).substring(2);
@@ -39,7 +40,7 @@ app.get('/callback', async (req, res) => {
   const { code, state } = req.query;
 
   if (!code) {
-    return res.status(400).send('Authorization code is missing');
+    return res.status(400).send('Authorization code missing');
   }
 
   const tokenUrl = 'https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token';
@@ -60,15 +61,55 @@ app.get('/callback', async (req, res) => {
       }
     );
 
-    console.log('Token Response:', response.data);
-    res.json(response.data);
+    accessToken = response.data.access_token;
+    console.log('Token saved');
+
+    // Redirect back to frontend after login
+    res.redirect('http://miviewer.com');
   } catch (e) {
-    console.error('Token Exchange Error:', e.response?.data || e.message);
-    res
-      .status(500)
-      .send('Token exchange failed: ' + JSON.stringify(e.response?.data || e.message));
+    console.error('Token Exchange Error');
+    res.status(500).send('Failed token exchange');
   }
 });
 
+app.get('/get-wado-url', async (req, res) => {
+
+  if (!accessToken) {
+    return res.status(401).json({error: 'Unauthorized'});
+  }
+
+  const binaryId = 'fi-.ZBIQGr6kBi4oQDsITp83vZlQxo1TryPXICsld70k4';
+  const binaryUrl = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Binary/${binaryId}`;
+
+  try {
+    const response = await axios.get(binaryUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Accept: 'application/fhir+json',
+      }
+    });
+
+    const base64RTF = response.data.data;
+    const decodedRTF = Buffer.from(base64RTF, 'base64').toString('utf8');
+    const match = decodedRTF.match(/https:\/\/[^\s\\]+/);
+    const wadoUrl = match ? match[0] : null;
+
+    if (!wadoUrl) {
+      return res.status(500).json({error: 'Failed to extract WADO URL'});
+    }
+
+    res.json({ wadoUrl });
+
+  } catch (err) {
+    res.status(500).json({error: 'Failed to fetch WADO URL'});
+  }
+});
+
+app.get('/access-token', (req, res) => {
+  if (!accessToken) {
+    return res.status(401).json({error: 'Unauthorized'});
+  }
+  res.json({ accessToken });
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
